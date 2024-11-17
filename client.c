27b1042f370a3636f4ddad21847708e48f99af3d
0x90verflow <vlarrieu95130@gmail.com>
2024-11-17 13:28:21 +0100

    Update client.c

--- client.c
+++ client.c
@@ -3,15 +3,15 @@
 #include <signal.h>
 #include <unistd.h>
 
-// 0 -> SIGUSR2 et 1 -> SIGUSR1
+
 void send_char(int pid, char c) {
     for (int i = 0; i < 8; i++) {
         if ((c >> (7 - i)) & 1) {
-            kill(pid, SIGUSR1);  // Si le bit est 1, envoie SIGUSR1
+            kill(pid, SIGUSR1);
         } else {
-            kill(pid, SIGUSR2);  // Si le bit est 0, envoie SIGUSR2
+            kill(pid, SIGUSR2);
         }
-        usleep(500);  // Eviter les collisions
+        usleep(700);  // pour éviter les collisions
     }
 }
 
@@ -25,10 +25,9 @@ int main(int argc, char *argv[]) {
     char *message = argv[2];
 
     for (int i = 0; message[i] != '\0'; i++) {
-        send_char(pid, message[i]);  // Envoie chaque caractère
+        send_char(pid, message[i]);
     }
 
-    // Pour afficher le buffer
     kill(pid, SIGTERM);
 
     return 0;
